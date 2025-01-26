b963dc48cf52b872cce08523427c801b9079f31d
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-26 19:37:28 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,56 @@
-import java.util.Scanner;
-
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
 
-    public static long factorial() {
+    public static void main(String[] args) {
 
-        System.out.println("Enter a number to be factorial.");
-        long n = scanner.nextLong();
+        /*
+        here I will try my best to learn Big O which is essential for Coding interviews 
+        and common nowledge for Algorithms and Datastructures
+        */
+            
+        tries(new int[] {1, 2, 3, 4});
 
-        if (n == 0 || n == 1) {
-            return 1;
+    }
+
+
+    //Big O(N^3), because 3 loops
+    public static int sumCharCodes(String n) {
+        int sum = 0;
+
+        for (int i = 0; i < n.length(); i++) {
+            for (int j = 0; j < n.length(); j++) {
+                for (int k = 0; k < n.length(); k++) {
+                    System.out.println(sum);
+                    sum += n.charAt(i); // Add the ASCII value of the character
+                }
+            }
         }
 
-        long sum = 1;
-        for (int i = 1; i <= n; i++) {
-            sum *= i;
-        }
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(factorial());
+    //Big O(N^2), because 2 loops
+    public static int sumCharCodes2(String n) {
+        int sum = 0;
+
+        for (int i = 0; i < n.length(); i++) {
+            for (int j = 0; j < n.length(); j++) {
+                System.out.println(sum);
+                sum += n.charAt(i); // Add the ASCII value of the character
+            }
+        }
+
+        return sum;
+    }
+
+    //Big O(N), because 1 loop, N is the nums array
+    public static int tries(int[] nums) {
+        int tries = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+            tries++;
+        }
+
+        return tries;
     }
 }
